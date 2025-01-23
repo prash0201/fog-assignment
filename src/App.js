605969c295c851c84093b45b5c5d933ab737d1dc
prash0201 <0201prashantjaiswal@gmail.com>
2025-01-23 20:18:09 +0530

    refactoring done

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import FilterAndSortSection from "./components/FilterAndSortSection";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
-import Products from "./components/Products";
 import ShopSection from "./components/ShopSection";
 function App() {
   return (
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,7 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 import Card from "../components/Card";
-import axios from "axios";
 
 const Products = ({ cardDetails }) => {
   return (
